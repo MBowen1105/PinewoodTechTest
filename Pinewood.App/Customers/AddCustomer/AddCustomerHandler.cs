252cc65217e292e56ff9b8e63f1050723ac2bd11
Mark.Bowen <TheRealMarkBowen@gmail.com>
2024-06-23 12:21:16 +0100

    Added AddCustomerHandler Added Interface for CustomerService

---
+++ Pinewood.App/Customers/AddCustomer/AddCustomerHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Pinewood.Domain.Customers;
+using Pinewood.Domain.Shared;
+
+namespace Pinewood.App.Customers.AddCustomer
+{
+    public sealed class AddCustomerHandler(
+        ICustomerService CustomerService)
+               : IRequestHandler<AddCustomerCommand,
+           Result<AddCustomerCommandResponse>>
+    {
+        public async Task<Result<AddCustomerCommandResponse>> Handle(
+            AddCustomerCommand command,
+            CancellationToken cancellationToken)
+        {
+            var newCustomer = new Customer { Name = command.Name };
+
+            var result = await CustomerService.AddAsync(newCustomer, cancellationToken);
+
+            await CustomerService.SaveChangesAsync();
+
+            var addCustomerCommandResponse = new AddCustomerCommandResponse(result.Id);
+
+            return Result<AddCustomerCommandResponse>.Success(addCustomerCommandResponse);
+        }
+    }
+}
---
+++ Pinewood.Domain/Customers/ICustomerService.cs
@@ -0,0 +1,9 @@
+﻿namespace Pinewood.Domain.Customers
+{
+    public interface ICustomerService
+    {
+        Task<Customer> AddAsync(Customer customer, CancellationToken cancellationToken);
+
+        Task<int> SaveChangesAsync();
+    }
+}
